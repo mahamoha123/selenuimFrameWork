9a2d5be83410899b82a8b5ec30a55dc63934459a
mahamoha123 <maha.mohamed.abdelhamiid@gmail.com>
2022-06-03 12:29:57 +0200

    commit N1.0

--- src/test/java/Tests/TestBase.java
+++ src/test/java/Tests/TestBase.java
@@ -3,6 +3,10 @@ package Tests;
 import Utilities.Helper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -22,6 +26,25 @@ public class TestBase {
             System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver.exe");
             driver = new FirefoxDriver();
         }
+        //HeadlessBrowser
+        else if(browserName.equalsIgnoreCase("headless")){
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setJavascriptEnabled(true);
+            caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,System.getProperty("user.dir")
+                    +"/drivers/phantomjs.exe");
+            String[] phantomJsArgs = {"--web-security=no","--ignore-ssl-errors=yes"};
+            caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS,phantomJsArgs);
+            driver = new PhantomJSDriver(caps);
+        }
+        else if(browserName.equalsIgnoreCase("chrome-headless")){
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--window-size=1920,1080");
+            driver = new ChromeDriver(options);
+
+        }
+
 
 
         driver.manage().window().maximize();
